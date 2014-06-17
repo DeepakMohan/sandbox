fbfb0e7b4e4e0c8bf216e5ac60fc074a60c6b742
Deepak Mohanakrishnan <dpk.mohan1@gmail.com>
2014-06-16 20:58:21 -0400

    more scala

--- sandbox/src/com/deepak/sandbox/ClassUsage.scala
+++ sandbox/src/com/deepak/sandbox/ClassUsage.scala
@@ -5,5 +5,10 @@ object ComplexNumbers {
   def main(args: Array[String]) {
 	  val complex = new Complex(1.5, 2.5);
 	  println("Imaginery part: " + complex.im);
+	  println("Complex number: " + complex);
+	  
+	  val complex2 = new Complex(1.5, -2.5);
+	  println("Imaginery part: " + complex2.im);
+	  println("Complex number: " + complex2);
   }
 }
--- sandbox/src/com/deepak/sandbox/Complex.scala
+++ sandbox/src/com/deepak/sandbox/Complex.scala
@@ -3,4 +3,7 @@ package com.deepak.sandbox
 class Complex(real: Double, imaginary: Double) {
   def re = real
   def im = imaginary;
+  
+  override def toString() =
+    "" + (re) + (if(im > 0) " + " else " ") + im + "i";
 }
